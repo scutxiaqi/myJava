da683fe293a9633c1c9afb9001e51a514dfd0693
xiaqi <181623555@qq.com>
2020-07-28 16:37:43 +0800

    面试文件

--- src/main/java/com/xiaqi/myJava/java/concurrent/threadpool/Demo.java
+++ src/main/java/com/xiaqi/myJava/java/concurrent/threadpool/Demo.java
@@ -1,7 +1,9 @@
 package com.xiaqi.myJava.java.concurrent.threadpool;
 
+import java.util.concurrent.Executors;
+
 public class Demo {
     public static void main(String[] args) {
-        
+        Executors.newCachedThreadPool();
     }
 }
