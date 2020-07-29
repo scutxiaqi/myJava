1fd2be74b167aa8f11714956115d66bba02b7bb2
xiaqi <181623555@qq.com>
2020-07-29 08:51:28 +0800

    final

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 java学习笔记
 
 token: b838d16232271388786c5198a7a86ad707a5c363 
+
+
+
+
+sourceTree  qq邮箱，密码是8位数
---
+++ src/main/java/com/xiaqi/myJava/java/concurrent/threadpool/ScheduledThreadPool.java
@@ -0,0 +1,35 @@
+package com.xiaqi.myJava.java.concurrent.threadpool;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledThreadPool {
+
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
+        scheduleAtFixedRate(service, 1000);
+        // scheduleAtFixedRate(service,6000);
+    }
+
+    private static void scheduleAtFixedRate(ScheduledExecutorService service, final int sleepTime) {
+        service.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                long start = new Date().getTime();
+                System.out.println("scheduleAtFixedRate 开始执行时间:" + DateFormat.getTimeInstance().format(new Date()));
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                long end = new Date().getTime();
+                System.out.println("scheduleAtFixedRate 执行花费时间=" + (end - start) / 1000 + "m");
+                System.out.println("scheduleAtFixedRate 执行完成时间：" + DateFormat.getTimeInstance().format(new Date()));
+                System.out.println("======================================");
+            }
+        }, 1000, 5000, TimeUnit.MILLISECONDS);
+    }
+}
