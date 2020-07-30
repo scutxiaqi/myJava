36be267be702a2fe9cf216a568265d3db12cd39f
xiaqi <181623555@qq.com>
2020-07-30 18:30:57 +0800

    CopyOnWriteArrayList8

---
+++ src/main/java/com/xiaqi/myJava/java/collection/list/CopyOnWriteArrayList8.java
@@ -0,0 +1,88 @@
+package com.xiaqi.myJava.java.collection.list;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 线程安全的ArrayList.适合场景：读多写少。不适合大数据量(会造成频繁GC)
+ * 原理：运用了一种“ 写时复制”的思想：
+ * 通俗的理解就是当我们需要修改（增/删/改）列表中的元素时，不直接进行修改，而是先将列表Copy，然后在新的副本上进行修改，修改完成之后，再将引用从原列表指向新列表。
+ * 
+ * 优点：读操作不用加锁，读操作还是在原列表，写操作在新列表
+ */
+public class CopyOnWriteArrayList8<E> {
+    /**
+     * 排它锁, 用于同步修改操作
+     */
+    final transient ReentrantLock lock = new ReentrantLock();
+    /**
+     * 内部数组
+     */
+    private transient volatile Object[] array;
+
+    final void setArray(Object[] a) {
+        array = a;
+    }
+
+    final Object[] getArray() {
+        return array;
+    }
+
+    /**
+     * Creates an empty list.
+     */
+    public CopyOnWriteArrayList8() {
+        setArray(new Object[0]);
+    }
+
+    public boolean add(E e) {
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            Object[] elements = getArray();
+            int len = elements.length;
+            // 拷贝array到一个新数组, 新数组的大小是原来数组大小加增加1,所以CopyOnWriteArrayList是无界List
+            Object[] newElements = Arrays.copyOf(elements, len + 1);
+            newElements[len] = e;
+            setArray(newElements);
+            return true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public E remove(int index) {
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            Object[] elements = getArray();
+            int len = elements.length;
+            E oldValue = get(elements, index);
+            int numMoved = len - index - 1;
+            if (numMoved == 0)
+                setArray(Arrays.copyOf(elements, len - 1));
+            else {
+                Object[] newElements = new Object[len - 1];
+                System.arraycopy(elements, 0, newElements, 0, index);
+                System.arraycopy(elements, index + 1, newElements, index, numMoved);
+                setArray(newElements);
+            }
+            return oldValue;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public E get(int index) {
+        return get(getArray(), index);
+    }
+
+    @SuppressWarnings("unchecked")
+    private E get(Object[] a, int index) {
+        return (E) a[index];
+    }
+
+    public int size() {
+        return getArray().length;
+    }
+}
