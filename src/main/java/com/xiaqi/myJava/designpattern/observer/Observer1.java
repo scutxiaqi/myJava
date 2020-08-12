e7b41845742e8cd9da33cb8ae8195f3d6e8e7ba8
scutxiaqi <181623555@qq.com>
2020-08-13 07:39:07 +0800

    observer

--- src/main/java/com/xiaqi/myJava/designpattern/observer/Observer1.java
+++ src/main/java/com/xiaqi/myJava/designpattern/observer/Observer1.java
@@ -7,27 +7,7 @@ import java.util.Observer;
  * 观察者1
  */
 public class Observer1 implements Observer{
-	private Observable observable;
-
-	public Observer1(Observable observable) {
-		this.observable = observable;
-		observable.addObserver(this);
-	}
-
 	public void update(Observable obs, Object arg) {
-		if (obs instanceof Subject) {
-			Subject observable = (Subject) obs;
-			display();
-		}
-	}
-
-	public void display() {
-		System.out.println("有新数据了");
-	}
-	
-	public static void main(String[] args) {
-		Subject observable = new Subject();
-		Observer observer = new Observer1(observable);
-		observable.setMeasurements(11, 11, 11);
+	    System.out.println(arg);
 	}
 }
--- src/main/java/com/xiaqi/myJava/designpattern/observer/Subject.java
+++ src/main/java/com/xiaqi/myJava/designpattern/observer/Subject.java
@@ -5,32 +5,14 @@ import java.util.Observable;
 /**
  * 被观察对象
  */
-// 这是一个气象监测站，每当监测到数据，通知观察者
 public class Subject extends Observable {
-    private float temperature;// 温度
-    private float humidity;// 湿度
-    private float pressure;// 气压
-
     /**
-     * 重新设定了测量值
+     * 观察者模式：
      */
-    public void setMeasurements(float temperature, float humidity, float pressure) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        setChanged(); // 设定改变了
-        notifyObservers(); // 通知所有的观察者
-    }
-
-    public float getTemperature() {
-        return temperature;
-    }
-
-    public float getHumidity() {
-        return humidity;
-    }
-
-    public float getPressure() {
-        return pressure;
-    }
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        subject.addObserver(new Observer1());// 添加观察者，可以添加多个
+        subject.setChanged();// 被观察对象有变动了
+        subject.notifyObservers("love xiaqi");// 通知观察者
+      }
 }
